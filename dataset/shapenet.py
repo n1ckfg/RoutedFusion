8672299aac461c66cb2c01f6f25fb86c1a03c5bc
Nick Fox-Gieg <nick@fox-gieg.com>
2021-06-07 15:47:41 -0400

    Update shapenet.py

--- dataset/shapenet.py
+++ dataset/shapenet.py
@@ -7,7 +7,7 @@ from skimage import io, transform
 from torch.utils.data import Dataset
 from copy import copy
 
-from voxelgrid import Voxelgrid
+import Voxelgrid
 
 from scipy.ndimage.morphology import binary_dilation
 
