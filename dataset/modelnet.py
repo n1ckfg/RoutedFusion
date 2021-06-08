5f87a71e60cd8bf8dbbce970147cac07fb8fb543
Nick Fox-Gieg <nick@fox-gieg.com>
2021-06-07 23:11:33 -0400

    Update modelnet.py

--- dataset/modelnet.py
+++ dataset/modelnet.py
@@ -6,7 +6,7 @@ from skimage import io, transform
 from torch.utils.data import Dataset
 from copy import copy
 
-from graphics import Voxelgrid
+from .voxelgrid import Voxelgrid
 
 from scipy.ndimage.morphology import binary_dilation
 
